f61f60d1a686031bfb88cb143cbbb2c3a8f1a090
Muhammed Obari <obariomo@gmail.com>
2022-04-07 17:21:12 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+
+
+
 /**
+ *
  *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ *
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *
+ *    * Return: 0
+ *
  *     */
+
+
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
+
+
