d480990275e0fe44b25ed9f130bb94c54ca7f352
Muhammed Obari <obariomo@gmail.com>
2022-04-07 19:16:19 -0700

    fg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-
 {
 
 		printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -17,7 +16,6 @@ int main(void)
 	        printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 
 		printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
 		return (0);
 
 }
