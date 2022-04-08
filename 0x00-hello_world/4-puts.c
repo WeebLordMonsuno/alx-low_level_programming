a5f641c0ebbe223681777546dc685eb0c6713a59
Muhammed Obari <obariomo@gmail.com>
2022-04-07 17:38:35 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ *  * main - main block
  *
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *   * Return: 0
  *
- *    * Return: 0
- *
- *     */
-
-
+ *    */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	        puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		        return (0);
 
 }
-
-
