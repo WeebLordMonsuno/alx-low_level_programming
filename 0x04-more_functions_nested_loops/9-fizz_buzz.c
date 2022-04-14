da993c1971cbca9b43c5b32ca55cb5a855435807
Muhammed Obari <101449678+WeebLordMonsuno@users.noreply.github.com>
2022-04-14 22:35:57 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,40 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Entry piont
- * Return: Always 0
+ * main - Fizz Buzz code
+ *
+ * Return: void
  */
+
 int main(void)
 {
 	int i = 1;
 
-	for (; i < 100 ; i++)
+	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+		{
+			printf("FizzBuzz");
+		}
 		else if (i % 3 == 0)
-			printf("Fizz ");
+		{
+			printf("Fizz");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("Buzz");
+		}
 		else
-			printf("%d ", i);
-	}
-	printf("Buzz\n");
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
 
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
