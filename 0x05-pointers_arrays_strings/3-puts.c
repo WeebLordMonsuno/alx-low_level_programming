75146a6e31928ef7dc964df028ec39b3753fee71
Muhammed Obari <101449678+WeebLordMonsuno@users.noreply.github.com>
2022-04-21 17:22:50 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,20 @@
-chmod u+x 3-puts.c
+#include "main.h"
+#include <string.h>
+
+/**
+ *  * _strlen - checks the length of a string
+ *   * @s:input
+ *    * Return: Always 0.
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != 0)
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
