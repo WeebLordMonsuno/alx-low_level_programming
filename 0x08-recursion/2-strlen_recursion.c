aded6036fa504c0e1f9fa98d380d0e4d032dee4c
Muhammed Obari <101449678+WeebLordMonsuno@users.noreply.github.com>
2022-04-28 23:08:12 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,12 @@
-include "main.h"
-
+#include "main.h"
 /**
- * _strlen_recursion - main - check the code
- * @s: character of string
- * Return: Always 0.
- */
+ * _strlen_recursion - finds the length of a string.
+ * @s: pointer to string
+ * Return: length of string
+ **/
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
-	return (0);
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
 }
