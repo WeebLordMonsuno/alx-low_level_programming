21f142586ccd4bbb63c9ef50b38d488bb4fb05f4
Muhammed Obari <101449678+WeebLordMonsuno@users.noreply.github.com>
2022-05-24 23:09:51 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,9 @@
-#include "lists.h"
+#includists.h"
 #include <stdio.h>
 
 /**
  * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
+ * @h: The li
  */
 
 size_t print_list(const list_t *h)
@@ -19,10 +17,4 @@ size_t print_list(const list_t *h)
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
 }
