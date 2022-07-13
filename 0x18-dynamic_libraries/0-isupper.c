b6b5124de51ca027dd48aaba3b739ea14880bac4
Muhammed Obari <101449678+WeebLordMonsuno@users.noreply.github.com>
2022-07-13 01:00:24 +0100

    upperc

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,1 +1,11 @@
-sjskdjff
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @letter: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int letter)
+{
+return (letter >= 'A' && letter <= 'Z');
+}
